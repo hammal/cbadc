fb1f4515f5b61500e083a11ef1329a22e6a04b38
hammal <hampus.malmberg88@gmail.com>
2021-03-22 09:42:23 +0100

    update licence field in setup.py

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/hammal/cbadc",
     packages=['cbadc'],
+    license="GPL",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved ",
