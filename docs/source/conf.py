6d2340d584d2784784849fe8cf9cba2a2999e472
Hampus Malmberg <hampus.malmberg88@gmail.com>
2021-04-16 14:46:50 +0200

    more changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,8 @@ copyright = "2021, Hampus Malmberg"
 author = "Hampus Malmberg"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
-version = "0.0.1"
+# release = "0.0.3"
+# version = "0.0.3"
 
 
 # -- General configuration ---------------------------------------------------
