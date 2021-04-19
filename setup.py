d80864decec3966534ba65515c254b4eee935359
Hampus Malmberg <hampus.malmberg88@gmail.com>
2021-04-19 12:04:15 +0200

    windows test

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("README.md", "r") as fh:
 
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
-USING_CYTHON = os.environ.get('CYTHON', True)
+USING_CYTHON = os.environ.get('CYTHON', False)
 print(f"Using Cython?: {USING_CYTHON}")
 ext = "pyx"
 
