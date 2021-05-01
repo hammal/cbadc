20eeb698cca47846330ddc32524febec463a5008
hammal <hampus.malmberg88@gmail.com>
2021-05-01 23:38:24 +0200

    additional unittests

---
+++ tests/benchmark/test_simulator/pre_simulation_test.py
@@ -0,0 +1,33 @@
+import numpy as np
+from cbadc.digital_estimator import DigitalControl, ParallelEstimator, \
+    IIRFilter, FIRFilter
+from cbadc.analog_system import AnalogSystem
+from cbadc.analog_signal import Sinusodial
+from cbadc.simulator import StateSpaceSimulator
+
+beta = 6250.0
+rho = -62.5
+N = 10
+A = np.eye(N) * rho + np.eye(N, k=-1) * beta
+B = np.zeros((N, 1))
+B[0, 0] = beta
+# B[0, 1] = -beta
+C = np.eye(N)
+Gamma_tilde = np.eye(N)
+Gamma = Gamma_tilde * (-beta)
+Ts = 1/(2 * beta)
+
+
+eta2 = 1e6
+K1 = 1 << 12
+K2 = 1 << 12
+size = K2 << 4
+analogSystem = AnalogSystem(A, B, C, Gamma, Gamma_tilde)
+digitalControl = DigitalControl(Ts, N)
+analogSignals = [Sinusodial(0.5, 1)]
+
+
+def test_pre_simulation_algorithm(benchmark):
+    def setup():
+        StateSpaceSimulator(analogSystem, digitalControl, analogSignals)
+    benchmark(setup)
