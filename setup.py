9fa862a1c9c18b8ba8f7d3d016595441c7bfd19d
hammal <hampus.malmberg88@gmail.com>
2021-05-03 13:08:39 +0200

    update to v.0.0.15

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ else:
 
 setup(
     name="cbadc",
-    version="0.0.14",
+    version="0.0.15",
     author="Hampus Malmberg",
     author_email="hampus.malmberg88@gmail.com",
     description="A toolbox for simulating control-bounded converters.",
