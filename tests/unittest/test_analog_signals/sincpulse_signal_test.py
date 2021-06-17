28863bf8cabfaffaade5569b14a930cc29dacc9a
kasettli <info@kasett.li>
2021-06-17 19:03:21 +0200

    fix and improve sinc unittest

--- tests/unittest/test_analog_signals/sincpulse_signal_test.py
+++ tests/unittest/test_analog_signals/sincpulse_signal_test.py
@@ -1,3 +1,5 @@
+from numpy.testing import assert_almost_equal
+from numpy import linspace 
 from cbadc.analog_signal import SincPulse
 import math
 
@@ -9,17 +11,22 @@ def test_initialization():
     SincPulse(amplitude, bandwidth, delay)
 
 
-def test_evaluate():
+def test_evaluate_multiple():
     amplitude = 1.2
     bandwidth = 42.
-    delay = 11
-    t = 1
+    delay = math.pi
     sinc = SincPulse(amplitude, bandwidth, delay)
-    assert sinc.evaluate(t) == (amplitude *
-                                    math.sin(2 * math.pi * bandwidth * (t-delay)) /
-                                    (2 * math.pi * bandwidth * (t-delay))
-                                ) 
 
+    eval_times = linspace(-9.9, 9,8, 50)      # test at 50 different pointss
+    for t in eval_times:
+        if (abs(t) < 1e-15):
+            assert_almost_equal(sinc.evaluate(t), amplitude, decimal=10)
+        else: 
+            assert_almost_equal(sinc.evaluate(t),
+                                        ( amplitude * 
+                                        math.sin(2 * math.pi * bandwidth * (t-delay)) / 
+                                        (2 * math.pi * bandwidth * (t-delay)) ),    
+                                    decimal=10)
 
 def test_evaluate_with_offset():
     amplitude = 1.2
@@ -27,11 +34,12 @@ def test_evaluate_with_offset():
     delay = 20
     offset = 4.5321
     t = 3.7
-    sinc = SincPulse(amplitude, bandwidth, delay)
-    assert sinc.evaluate(t) == (amplitude *
-                                    math.sin(2 * math.pi * bandwidth * (t-delay)) /
-                                    (2 * math.pi * bandwidth * (t-delay)) + offset
-                                )
+    sinc = SincPulse(amplitude, bandwidth, delay, offset)
+    assert_almost_equal(sinc.evaluate(t),
+                            (amplitude *
+                             math.sin(2 * math.pi * bandwidth * (t-delay)) /
+                            (2 * math.pi * bandwidth * (t-delay)) + offset ),
+                        decimal=10)
 
 
 def test_properties():
@@ -44,3 +52,4 @@ def test_properties():
     assert sinc.bandwidth == bandwidth
     assert sinc.delay == delay
     assert sinc.offset == offset
+
