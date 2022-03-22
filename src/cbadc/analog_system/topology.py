b714f289b96b87b3175d92401f65d0233a33f8bf
Fredrik Feyling <fredrik.e.feyling@ntnu.no>
2022-03-22 17:52:21 +0100

    Local control on ctsd modulator

--- src/cbadc/analog_system/topology.py
+++ src/cbadc/analog_system/topology.py
@@ -405,7 +405,7 @@ def tf2abcd(
 
 
 def ctsd2abc(ctsd_dict: Dict, T):
-    """Build A, B, C matrices from a dictionary describing a continuous-time delta-sigma modulator.
+    """Build A, B and CT matrices from a dictionary describing a continuous-time delta-sigma modulator.
 
     Parameters
     ----------
@@ -415,11 +415,11 @@ def ctsd2abc(ctsd_dict: Dict, T):
         Desired control period, used for scaling the coefficients of the system.
 
     Returns
-    A: numpy.ndarray, shape=(L, L)
+    A: numpy.ndarray, shape=(N, N)
         state transition matrix.
-    B: numpy.ndarray, shape=(L, 1)
+    B: numpy.ndarray, shape=(N, 1)
         input matrix.
-    CT: numpy.ndarray, shape=(1,L)
+    CT: numpy.ndarray, shape=(N,N)
         signal observation matrix.
     """
     N = ctsd_dict['systemOptions']['systemOrder']
@@ -430,19 +430,76 @@ def ctsd2abc(ctsd_dict: Dict, T):
     CT = np.eye(N)
     # Build A, B. Multiply by fs
     for n1 in range(N):
-        if f'b{n1+1}' in coefficients:
-            B[n1, 0] = coefficients[f'b{n1+1}']['fixedValue'] * fs
+        B[n1, 0] = _get_coeff_value(coefficients, f'b{n1+1}') * fs
         if f'c{n1}' in coefficients:
-            A[n1, n1 - 1] = coefficients[f'c{n1}']['fixedValue'] * fs
+            A[n1, n1 - 1] = _get_coeff_value(coefficients, f'c{n1}') * fs
         for n2 in range(N):
             if f'e{n2+1}{n1+1}' in coefficients:
-                A[n1, n2] = coefficients[f'e{n2+1}{n1+1}']['fixedValue'] * fs
+                A[n1, n2] = _get_coeff_value(coefficients, f'e{n2+1}{n1+1}') * fs
             if f'd{n2+1}{n1+1}' in coefficients:
-                A[n1, n2] = coefficients[f'd{n2+1}{n1+1}']['fixedValue'] * fs
+                A[n1, n2] = _get_coeff_value(coefficients, f'd{n2+1}{n1+1}') * fs
     return A, B, CT
 
 
-def ctsd2af(ctsd_dict: Dict, T, dac_scale):
+def ctsd2gamma(ctsd_dict: Dict, T, dac_scale, local_control=True):
+    """Build Gamma and Gamma_tildeT matrices from a dictionary describing a continuous-time delta-sigma modulator.
+    Keep the scaling from the CTSD. Choose between local and global control, and the number of quuantizer levels.
+
+    Parameters
+    ----------
+    path: Dict
+        Dictionary describing the continuous-time delta-sigma modulator. The dictionary is assumed to have the same format as the json files exported from `www.sigma-delta.de <www.sigma-delta.de>`_ . The coefficients are assumed to be normalized to the sampling frequency, and will be scaled according to the specified control period T.
+    T: float
+        Desired control period, used for scaling the coefficients of the system.
+    local_control: Bool
+        Whether or not to use local control. With local control, the scaling is kept for the Gamma matrix, but Gamma_tildeT is set to the identity matrix
+
+    Returns
+    Gamma: numpy.ndarray, shape=(N, M)
+        Control input matrix
+    Gamma_tildeT: numpy.ndarray, shape=(M_tilde, N)
+        Control observation matrix
+    D_tilde: numpy.ndarray, shape=(1,1)
+        signal observation matrix.
+    """
+    N = ctsd_dict['systemOptions']['systemOrder']
+    M = N if local_control else 1
+    M_tilde = M
+    fs = 1 / T
+
+
+    coefficients = ctsd_dict['coefficient']
+    # Build Gamma, multiply by fs and dac_scale
+    Gamma = np.zeros((N, M))
+    for n1 in range(N):
+        n2 = n1 if local_control else 0
+        Gamma[n1, n2] = _get_coeff_value(coefficients, f'a{n1+1}') * fs * dac_scale
+    # Build Gamma_tildeT and D_tilde
+    if local_control:
+        # Ignore observation from ctsd dict if local control
+        D_tilde = np.zeros((M_tilde, 1))
+        Gamma_tildeT = np.eye(M)
+    else:
+        # Try to pick last c value for gamma tilde, use 1 as default
+        # Gamma_tilde is a pure scale factor, no integration
+        Gamma_tildeT = np.zeros((M_tilde, N))
+        Gamma_tildeT[0, -1] = _get_coeff_value(coefficients, f'c{N}', default=1)
+        # Build D_tilde
+        D_tilde = np.zeros((M_tilde, 1))
+        D_tilde[-1, 0] = _get_coeff_value(coefficients, f'b{N+1}')
+
+    return Gamma, Gamma_tildeT, D_tilde
+
+
+def _get_coeff_value(coefficients: Dict, coeff_name: str, default: float=0):
+    """ Return value if the coefficient exist and is active, else default """
+    try:
+        return coefficients[coeff_name]['fixedValue']
+    except KeyError:
+        return default
+
+
+def ctsd2af(ctsd_dict: Dict, T, dac_scale, local_control=False, qlev=None):
     """Construct an analog system and a digital control based on a dictionary describing a continuous-time delta-sigma modulator.
     .
         Parameters
@@ -453,6 +510,10 @@ def ctsd2af(ctsd_dict: Dict, T, dac_scale):
             Desired control period, used for scaling the coefficients of the system.
         dac_scale: float
             Additional scaling to apply to the DAC coefficients.
+        local_control: Bool
+            Whether or not to use local control. Default: False
+        qlev: Int
+            Number of quantizer levels. If None, use the number of levels from the ctsd dictionary. Default: None
 
         Returns
         analog_system: :py:class:`cbadc.analog_system.analog_system.AnalogSystem`
@@ -460,29 +521,16 @@ def ctsd2af(ctsd_dict: Dict, T, dac_scale):
         digital_control: :py:class:`cbadc.digital_control.DigitalControl`
             Digital Control
     """
-    N = ctsd_dict['systemOptions']['systemOrder']
-    M = 1
-    fs = 1 / T
-    Qlev = ctsd_dict['quantizer']['level']
-
+    # Parse matrices
     A, B, CT = ctsd2abc(ctsd_dict, T)
-    Gamma_tildeT = np.zeros((M, N))
-    Gamma = np.zeros((N, M))
-    coefficients = ctsd_dict['coefficient']
-    # Build Gamma, multiply by fs and dac_scale
-    for n1 in range(N):
-        if f'a{n1+1}' in coefficients:
-            Gamma[n1, 0] = coefficients[f'a{n1+1}']['fixedValue'] * fs * dac_scale
-
-    # Try to pick last c value for gamma tilde
-    # Gamma_tilde is a pure scale factor, no integration
-    if f'c{N}' in coefficients:
-        Gamma_tildeT[0, -1] = coefficients[f'c{N}']['fixedValue']
-    else:
-        Gamma_tildeT[0, -1] = 1
-
+    Gamma, Gamma_tildeT, D_tilde = ctsd2gamma(ctsd_dict, T, dac_scale, local_control)
+    (M_tilde, _) = Gamma_tildeT.shape
+    (_, M) = Gamma.shape
     # Init AS
-    AS = AnalogSystem(A, B, CT, Gamma, Gamma_tildeT)
+    AS = AnalogSystem(A, B, CT, Gamma, Gamma_tildeT, D_tilde=D_tilde)
     # Init DC
-    DC = MultiLevelDigitalControl(Clock(T), M, [Qlev])
+    Qlevels = ctsd_dict['quantizer']['level'] if qlev is None else qlev
+    number_of_levels = [Qlevels for i in range(M_tilde)]
+    DC = MultiLevelDigitalControl(Clock(T), M, number_of_levels=number_of_levels)
     return AS, DC
+
