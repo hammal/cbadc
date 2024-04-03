7f7e759dc04631e9c45743a9d8c2b4c3f89877cb
Hampus Malmberg <hampa@me.com>
2024-04-03 15:29:00 +0000

    fixed reference error

--- tests/circuits/test_reference_source.py
+++ tests/circuits/test_reference_source.py
@@ -1,16 +1,23 @@
 from cbadc.circuit import Terminal, SubCircuitElement
 from cbadc.circuit.components.reference_source import ReferenceSource
+import numpy as np
 
 
 def test_comparator_ngspice():
-    terminals = [Terminal('clk'), Terminal('in'), Terminal('out')]
-    subckt = SubCircuitElement('Xsub', 'subckt', terminals)
+    terminals = [Terminal("clk"), Terminal("in"), Terminal("out")]
+    subckt = SubCircuitElement("Xsub", "subckt", terminals)
+    rng = np.random.default_rng()
+    random_sequence_length = 1 << 10
+    number_of_sources = 3
     subckt.add(
         ReferenceSource(
-            instance_name='Ars',
-            input_filename='input.txt',
-            number_of_sources=3,
-            model_name='ref',
+            instance_name="Ars",
+            input_filename="input.txt",
+            number_of_sources=number_of_sources,
+            model_name="ref",
+            pseudo_random_sequence=rng.integers(
+                2, size=(random_sequence_length, number_of_sources)
+            ),
         )
     )
 
@@ -22,13 +29,13 @@ def test_comparator_ngspice():
 
     print(subckt.Ars.get_ngspice(subckt._internal_connections))
     assert (
-        subckt.Ars.get_ngspice(subckt._internal_connections) == 'Ars [CLK IN OUT] ref'
+        subckt.Ars.get_ngspice(subckt._internal_connections) == "Ars [CLK IN OUT] ref"
     )
     print(subckt.get_ngspice(subckt._internal_connections))
-    assert subckt.get_ngspice(subckt._internal_connections) == 'Xsub CLK IN OUT subckt'
+    assert subckt.get_ngspice(subckt._internal_connections) == "Xsub CLK IN OUT subckt"
     print([model.get_ngspice() for model in subckt._get_model_set()][0])
-    print('\n\n')
+    print("\n\n")
     for subckt_definition in subckt.get_sub_circuit_definitions():
         print(subckt_definition)
-        print('\n\n')
+        print("\n\n")
     assert True
