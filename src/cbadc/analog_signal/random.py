Fixed Gaussian scaling (#161)
* fixed Gauss scaling

* added uniform reference signal

* semantics

---------

Co-authored-by: Hampus Malmberg <hampa@me.com>

src/cbadc/analog_signal/random.py
@@ -103,7 +103,7 @@ class GaussianReferenceSignal(_AnalogSignal):
         self.T = T
         self._buffer_size = 1 << 10
         self._buffer_index = 0
-        self._values = np.random.randn(self._buffer_size)
+        self._values = np.random.randn(self._buffer_size) * self.std + self.mean
 
     def __str__(self):
         return f"""Gaussian random signal with mean: {self.mean}, standard deviation: {self.std}, and period {self.T}"""
@@ -127,6 +127,68 @@ class GaussianReferenceSignal(_AnalogSignal):
             self._t = t
             self._buffer_index += 1
             if self._buffer_index == self._buffer_size:
-                self._values = np.random.randn(self._buffer_size)
+                self._values = np.random.randn(self._buffer_size) * self.std + self.mean
+                self._buffer_index = 0
+        return self._values[self._buffer_index]
+
+
+class UniformReferenceSignal(_AnalogSignal):
+    """
+    A Gaussian reference signal.
+
+    Parameters
+    ----------
+    T : `float`
+        The period of the signal.
+    low : `float`
+        the lower bound of the signal.
+    high : `float`
+        the upper bound (non inclusive) of the signal.
+    offset : `float`, `optional`
+        The offset of the signal, default is 0.0.
+    Example
+    -------
+    >>> from cbadc.analog_signal import UniformReferenceSignal
+    >>> u = UniformReferenceSignal(1, 0, 1)
+
+    """
+
+    def __init__(self, T: float, low: float, high: float, offset: float = 0.0):
+        self.low = low
+        self.high = high
+        self.offset = offset
+        self._t = 0.0
+        self.T = T
+        self._buffer_size = 1 << 10
+        self._buffer_index = 0
+        self._values = np.random.uniform(
+            self.low + self.offset, self.high + self.offset, self._buffer_size
+        )
+
+    def __str__(self):
+        return f"""Uniform random signal with values between: [{self.low+self.offset}, {self.high + self.offset}), and period {self.T}"""
+
+    def evaluate(self, t: float) -> float:
+        """
+        Evaluate the signal at time :math:`t`.
+
+        Parameters
+        ----------
+        t : `float`
+            the time instance for evaluation.
+
+        Returns
+        -------
+        float
+            The analog signal value
+        """
+        rel_t = t - self._t
+        if rel_t >= self.T:
+            self._t = t
+            self._buffer_index += 1
+            if self._buffer_index == self._buffer_size:
+                self._values = np.random.uniform(
+                    self.low + self.offset, self.high + self.offset, self._buffer_size
+                )
                 self._buffer_index = 0
         return self._values[self._buffer_index]